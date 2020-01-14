b8451c086f09c7859c7189824433061bff31b2ee
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-01-14 14:12:32 +0000

    Variables intro
    - concept of initialising variables
    - initialised several variables then affected them using different arithmatic operators
    - printed variables to the terminal
    - improvments to string 'storyline'

--- TripleXbits/TripleX.cpp
+++ TripleXbits/TripleX.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-    std:: cout << "Hello, World!";
+    //intro text
+    std:: cout << "You're trapped in a rather average room, the only plausible way out in a locked door across from you";
+    std:: cout << std:: endl; // adds a new line
+    std:: cout << "There's a number of keypads on the door. It seems that to unlock the door you will need to enter the correct codes in sequence...";
+    std:: cout << std:: endl;
+    
+    // first lock
+    int a = 4;
+    int b = 2;
+    int c = 1;
+
+    int sum = a + b + c; // adds each of the initialized variables together
+    int product = a * b * c;
+
+
+    std:: cout << sum << std:: endl; // prints sum (the total of a,b and c) to terminal
+    std:: cout << product << std:: endl;; // prints sum (the total of a,b and c) to terminal
     return 0;
 }
