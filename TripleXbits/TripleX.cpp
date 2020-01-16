0cfeff03a0f4e191e0bd941d04b263ffcee289b5
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-01-16 14:02:00 +0000

    if/else
    - basic intro to using if/else statements in C++
    - variable naming conventions added

--- TripleXbits/TripleX.cpp
+++ TripleXbits/TripleX.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     //intro text
-    std:: cout << "You're trapped in a rather average room, the only plausible way out in a locked door across from you";
+    std:: cout << "You're trapped in a rather average room, the only plausible way out seems to be a locked door across from you";
     std:: cout << std:: endl; // adds a new line
-    std:: cout << "There's a number of keypads on the door. It seems that to unlock the door you will need to enter the correct codes in sequence...";
+    std:: cout << "You notice a keypad on the door. It seems that to unlock the door you will need to enter the correct code in sequence..." << std:: endl;
     std:: cout << std:: endl;
     
     // first lock
@@ -13,10 +13,30 @@ int main()
     const int b = 2;
     const int c = 1;
 
-    const int sum = a + b + c; // adds each of the initialized variables together
-    const  int product = a * b * c;
+    const int CodeSum = a + b + c; // adds each of the initialized variables together
+    const  int CodeProduct = a * b * c;
+    std:: cout << "++ There are 3 numbers in the code" << std:: endl;
+    std:: cout << "++ The code adds up to: " << CodeSum << std:: endl; // prints sum (the total of a,b and c) to terminal
+    std:: cout << "++ And the the code multiplies to give: " <<CodeProduct << std:: endl; // prints sum (the total of a,b and c) to terminal
 
-    std:: cout << sum << std:: endl; // prints sum (the total of a,b and c) to terminal
-    std:: cout << product << std:: endl;; // prints sum (the total of a,b and c) to terminal
+    int GuessA, GuessB, GuessC;
+
+    std:: cin >> GuessA;
+    std:: cin >> GuessB;
+    std:: cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+{
+     std:: cout << "** Well Done, that was correct! **" << std:: endl; 
+}
+
+else
+{
+    std:: cout << "** That code was incorrect **" << std:: endl; 
+}
+
     return 0;
 }
