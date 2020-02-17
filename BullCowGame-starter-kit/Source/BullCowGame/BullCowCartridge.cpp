b8cdf328bc3d6ba73e6cfd09b94810b755f3d8e5
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-02-17 14:03:59 +0000

    Update BullCowCartridge.cpp
    - Intro to using strings in unreal + text macros
    -Basic accessing of user input + display of input using strings

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Hello there, Press Tab to use the Terminal\nThen press enter to continue..")); // Prints a line to the terminal
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen(); // clear the terminal
+    FString HiddenWord = TEXT("LIME");
+    PrintLine(TEXT ("You guessed: " + (Input)));
 }
