a7b419e0562402ced51b1914c30e210f079e9b6e
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-02-18 14:00:17 +0000

    members/instance variable
    - basic intro to instance variables
    - check for guess
    - refresh to header target code to improve complier time

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,13 +4,30 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Hello there, Press Tab to use the Terminal\nThen press enter to continue..")); // Prints a line to the terminal
-    
+    PrintLine(TEXT("Hello there, Press Tab to use the Terminal")); // Prints a line to the terminal
+    PrintLine(TEXT("Guess the 4 letter word\n Then press enter...")); // hard coded length of number but remove later
+    HiddenWord = TEXT("LIME");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen(); // clear the terminal
-    FString HiddenWord = TEXT("LIME");
     PrintLine(TEXT ("You guessed: " + (Input)));
+    
+        if (Input == HiddenWord)
+    {
+        PrintLine("Well done, that was correct!");
+    }
+
+    else
+    {
+      PrintLine ("That was incorrect, please try again");
+    }
+
+}
+
+void CheckGuess()
+{
+
+
 }
