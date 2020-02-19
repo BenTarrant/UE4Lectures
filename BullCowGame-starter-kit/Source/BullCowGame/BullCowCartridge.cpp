513800abfd3468ce0c75ca9fa99f05e4f0771c2f
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-02-19 14:01:02 +0000

    Function intro
    basic intro to writing a function
    pseudo coding to understand intended game loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,15 +3,23 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    //Begin Game
     Super::BeginPlay();
     PrintLine(TEXT("Hello there, Press Tab to use the Terminal")); // Prints a line to the terminal
     PrintLine(TEXT("Guess the 4 letter word\n Then press enter...")); // hard coded length of number but remove later
-    HiddenWord = TEXT("LIME");
+
+    SetupGame();
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen(); // clear the terminal
+
+    //check the guess
+    //is it an isogram?
+    //is is the right amount of characters?
+
     PrintLine(TEXT ("You guessed: " + (Input)));
     
         if (Input == HiddenWord)
@@ -22,12 +30,19 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     else
     {
       PrintLine ("That was incorrect, please try again");
+      //Lives -1;
+      PrintLine ((Lives) + "Lives Remaining ");
+      PrintLine ("Please guess again");
+
+      //remove a life
+      //show how many lives remaining
+      //prompt to guess again
     }
 
 }
 
-void CheckGuess()
+void UBullCowCartridge::SetupGame()
 {
-
-
+    HiddenWord = TEXT("LIME");
+    Lives = 3;
 }
