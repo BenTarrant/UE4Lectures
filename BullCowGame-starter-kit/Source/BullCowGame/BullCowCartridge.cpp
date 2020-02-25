a537d21c0e9a03adba7ab23ba3518a9aee64fb96
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-02-25 13:59:07 +0000

    update
    - refactored code to house the guess processing in its own function
    - corrected lives decrement to be a pre-decrement
    - refactored strings for greater clarity

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,13 +4,14 @@
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("LIME");
-    Lives = 3;
+    Lives = HiddenWord.Len();
     bGameOver = false; // ensure the game over flag is clear
 
     //Intro Text
     PrintLine(TEXT("The Hidden Word is: %s "), *HiddenWord); //debug line
     PrintLine(TEXT("Hello there, Press Tab to use the Terminal")); // Prints a line to the terminal
-    PrintLine(TEXT("Guess the %i letter word\n Then press enter..."), HiddenWord.Len());
+    PrintLine(TEXT("You need to guess the %i letter word\nYou have %i Attempts"), HiddenWord.Len(), Lives);
+    PrintLine(TEXT("Type in your Guess\nThen press enter..."));
 }
 
 void UBullCowCartridge::BeginPlay() // When the game starts
@@ -23,6 +24,32 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ProcessGuess(Input);
+}
+
+void UBullCowCartridge::LoseLife()
+{
+    if (Lives > 0)
+    {
+        --Lives; //minus a life
+
+        PrintLine (TEXT("That was incorrect"));  
+        PrintLine(TEXT("the word is %i letters long\nYou have %i Lives left"), HiddenWord.Len(), Lives);
+        PrintLine (TEXT("Please try a new guess"));  //prompt to guess again
+    }
+
+    else
+    {
+        PrintLine (TEXT("That was incorrect"));
+        PrintLine (TEXT("And you have run out of lives!\nGame Over"));
+        bGameOver = true; // set the game over flag
+        PrintLine (TEXT("Press Enter to Play again"));
+    } 
+}
+
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
     if (bGameOver != true) //if the game is not over
     {
         ClearScreen(); // clear the terminal
@@ -30,18 +57,22 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         //is it an isogram?
         //is is the right amount of characters?
 
-        PrintLine(TEXT ("You guessed: " + (Input)));
+        PrintLine(TEXT("You guessed: " + (Guess)));
     
-        if (Input == HiddenWord)
+        if (Guess == HiddenWord)
         {
-            PrintLine("Well done, that was correct!");
             // success state
+            PrintLine(TEXT("Well done, that was correct!"));
+            PrintLine(TEXT("You Win!"));
+            bGameOver = true; // set the game over flag
+            PrintLine (TEXT("Press Enter to Play again"));
+
         }
 
         else
         {
 
-            if (Input.Len() != HiddenWord.Len())
+            if (Guess.Len() != HiddenWord.Len())
             {
                 PrintLine (TEXT("The hidden word is %i characters long"), HiddenWord.Len());
             }
@@ -55,26 +86,4 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         ClearScreen(); // clear the terminal
         SetupGame(); // reset the game
     }
-
 }
-
-void UBullCowCartridge::LoseLife()
-{
-    if (Lives > 0)
-    {
-        Lives --; //minus a life
-
-        PrintLine (TEXT("That was incorrect"));  
-        PrintLine (TEXT("You have %i Lives Remaining"), Lives); //show how many lives remaining
-        PrintLine (TEXT("Please guess again"));  //prompt to guess again
-    }
-
-    else
-    {
-        PrintLine (TEXT("That was incorrect"));
-        PrintLine (TEXT("And you have run out of lives!\nGame Over"));
-        bGameOver = true; // set the game over flag
-        PrintLine (TEXT("Press Enter to Play again"));
-    } 
-}
-
