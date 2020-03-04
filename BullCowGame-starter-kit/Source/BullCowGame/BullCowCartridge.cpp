282397f51493277e6d41c51e132e01d1fbacec64
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-03-04 14:01:38 +0000

    Update BullCowCartridge.cpp
    - nested loop added to check letters aginst eachother

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -98,17 +98,17 @@ void UBullCowCartridge::LoseLife()
 
 bool UBullCowCartridge::IsIsogram(FString Word) const
 {
-    //curently only checking if the first letter of the guess is repeated!
-    int32 Index = 0;
-    int32 Comparison = Index +1;
-
-    for (; Comparison < Word.Len(); Comparison++)
+    
+    for (int32 Index = 0; Index < Word.Len(); Index++)
     {
-        if (Word[Index] == Word[Comparison])
+        for (int32 Comparison = Index +1; Comparison < Word.Len(); Comparison++)
         {
-            return false;
+            if (Word[Index] == Word[Comparison])
+            {
+                return false;
+            }
+            
         }
-        
     }
     
     return true;
