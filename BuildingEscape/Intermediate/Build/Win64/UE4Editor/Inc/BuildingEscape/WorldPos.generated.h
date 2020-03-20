4e9998be7b3b904d4336a4a8a7937a7a61cebc2a
Ben Tarrant <ben.g.tarrant@gmail.com>
2020-03-20 08:49:49 +0000

    Adding a Component
    - Basic creating and adding of an as yet empty class component to several actors in scece
    -Component called WorldPos

---
+++ BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/WorldPos.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BUILDINGESCAPE_WorldPos_generated_h
+#error "WorldPos.generated.h already included, missing '#pragma once' in WorldPos.h"
+#endif
+#define BUILDINGESCAPE_WorldPos_generated_h
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_RPC_WRAPPERS
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUWorldPos(); \
+	friend struct Z_Construct_UClass_UWorldPos_Statics; \
+public: \
+	DECLARE_CLASS(UWorldPos, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BuildingEscape"), NO_API) \
+	DECLARE_SERIALIZER(UWorldPos)
+
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUWorldPos(); \
+	friend struct Z_Construct_UClass_UWorldPos_Statics; \
+public: \
+	DECLARE_CLASS(UWorldPos, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BuildingEscape"), NO_API) \
+	DECLARE_SERIALIZER(UWorldPos)
+
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UWorldPos(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UWorldPos) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWorldPos); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWorldPos); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWorldPos(UWorldPos&&); \
+	NO_API UWorldPos(const UWorldPos&); \
+public:
+
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWorldPos(UWorldPos&&); \
+	NO_API UWorldPos(const UWorldPos&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWorldPos); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWorldPos); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UWorldPos)
+
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_PRIVATE_PROPERTY_OFFSET
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_10_PROLOG
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_PRIVATE_PROPERTY_OFFSET \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_RPC_WRAPPERS \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_INCLASS \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BuildingEscape_Source_BuildingEscape_WorldPos_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_PRIVATE_PROPERTY_OFFSET \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_INCLASS_NO_PURE_DECLS \
+	BuildingEscape_Source_BuildingEscape_WorldPos_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BUILDINGESCAPE_API UClass* StaticClass<class UWorldPos>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BuildingEscape_Source_BuildingEscape_WorldPos_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
